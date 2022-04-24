f05f3f9428ed61c6f79923111601e0bad93b7ee4
parien2022 <par22002@byui.edu>
2022-04-23 23:56:30 -0300

    testing

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const http = require("http");
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Nicolas Pariente\n");
+app.get("/", (req, res) => {
+  res.send("Alejandra Fros");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
