43f66605faeb33b3ef1229332aab06766184997d
nicolaspariente <par21022@byui.edu>
2022-04-25 08:27:06 -0300

    router folder and adding features

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const router = require("./router/router");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Alejandra Fros");
-});
+app.use("/", require("./router/router.js"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
